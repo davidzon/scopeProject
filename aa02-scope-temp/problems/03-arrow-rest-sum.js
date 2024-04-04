9565032a5d17d67f767f9923182a1dab49169833
Brendan <jbrendanwienold@gmail.com>
2024-04-04 18:56:20 -0500

    03 solved

--- aa02-scope-temp/problems/03-arrow-rest-sum.js
+++ aa02-scope-temp/problems/03-arrow-rest-sum.js
@@ -11,8 +11,14 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// Your code here 
-
+const arrowRestSum = (...nums) => {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum;
+};
+console.log(arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
