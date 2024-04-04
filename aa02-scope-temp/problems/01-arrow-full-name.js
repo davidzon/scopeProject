0d6fb913fd5522543c070ba31e89152da1575e53
Brendan <jbrendanwienold@gmail.com>
2024-04-04 18:44:30 -0500

    01 solved

--- aa02-scope-temp/problems/01-arrow-full-name.js
+++ aa02-scope-temp/problems/01-arrow-full-name.js
@@ -13,11 +13,19 @@ let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
 arrowGetFullName(p2); // => 'Charlie Brown'
 
 ***********************************************************************/
+/* Write a function using fat arrow syntax, `arrowGetFullName(person)` that takes in
+a person object and returns a string containing their full name.
 
-// Your code here
+Assign the below function to a variable using the const keyword. Using the const
+keyword will allow any value assigned to that variable protection from being
+reassigned within that scope. */
+let p1 = {firstName: 'John', lastName: 'Doe'};
 
+const arrowGetFullName = (person) => {
+  return person.firstName + ' ' + person.lastName;
+};
 
-
+console.log(arrowGetFullName(p1)); // => 'John Doe')
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
